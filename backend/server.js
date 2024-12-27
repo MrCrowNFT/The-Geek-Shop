536e1b123b52de83902bb96082d770c65a219a22
MrCrowNFT <mrcrownft@gmail.com>
2024-12-26 22:02:26 -0300

    User routes

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,29 @@ const PORT = process.env.PORT || 5500;
 
 app.use(express.json()); //to accept JSON files
 
+//USER ROUTES
 app.get("/", (req, res) =>{
     return res.status(200).send("Home Page")
 })
+app.get("/products", (req, res)=>{
+    return res.status(200).send("Products")
+})
+app.get("/products/:id", (req, res)=>{
+    const {id} = req.params;
+
+    //check if product is valid 
+
+    //find the product by id
+})
+app.get("/checkout", (req, res) => {
+
+})
+app.post("/checkout", (req, res)=>{
+    //add to orders clusters
+})
+
+
+//ADMIN ROUTES
 
 app.listen(PORT, ()=>{
     console.log("Server started at http://localhost:" + PORT)
