ead84bc62a449103049b0ac21972e6585033ea02
MrCrowNFT <mrcrownft@gmail.com>
2024-12-28 00:05:51 -0300

    Product schema

---
+++ backend/models/product.model.js
@@ -0,0 +1,77 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        //Price and cost calculculation properties
+        //need to check the math
+        //((cost + shipping)*(profit+tax))/discount
+        priceTag: {
+            type: Number,
+            required: true,
+        },
+        //price and shipping gotten from aliexpress
+        cost:{
+            type: Number,
+            required: true,
+        },
+        shipping:{
+            type: Number,
+            required: true,
+        },
+        profit:{
+            type: Number,
+            required: true,
+        },
+        tax:{
+            type: Number,
+            required: true,
+        },
+        discount:{
+            amount: {type: Number, required: false },
+            status: {type: Boolean, required: false},
+        },
+
+        availability:{
+            type: Boolean,
+            required: true,
+        },
+        //will be mainly use for google 
+        sku:{
+            type: Number,
+            required: true,
+        },
+        //the url might change in the future, since the primary function is for
+        //the to facilitate access to the product info in aliexpress
+        //to update availability, cost and price
+        //As of now the priority will determine the order in which the providers will be 
+        //consulted to fetch the info, i will only consult the next one if one is unavailable
+        urls:[{
+            url: { type: String, required: true },
+            priority: { type: Number, required: true }, 
+            required: true,
+        }],
+        images:[{
+            type: String,
+            required: true,
+        }],
+        description:{
+            type: String,
+            required: true,
+        },
+        category:[{
+            type: String,
+        }]
+
+    },
+    {
+      timestamps: true //createdAt, updatedAt
+    }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
