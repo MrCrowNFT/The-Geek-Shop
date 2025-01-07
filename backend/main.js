c4980f1eb5ac92c1fef12d90f3fe570a5d20efe9
MrCrowNFT <mrcrownft@gmail.com>
2025-01-06 22:53:57 -0300

    User methods

--- backend/main.js
+++ backend/main.js
@@ -5,7 +5,37 @@ const app = express();
 
 app.use(express.json()); //accept JSON files
 
-//USERS ROUTES
+//*USERS ROUTES
+//get the homepage
+app.get("/", (req, res)=>{
+    return res.status(200).send("Home page");
+})
+//get product by id
+app.get("/product/:id", (req, res)=>{
+    const {id} = req.params;
+    //check if the product id is valid
+    //find the product 
+    //return product
+
+    return res.status(200). send("Product")
+})
+
+//get the products list
+app.get("/products", (req, res)=>{
+    return res.status(200). send("Product")
+})
+
+//get search result page 
+app.get("/products/search", (req, res)=>{
+    return res.status(200). send("Product")
+})
+//get checkout page
+app.get("/checkout", (req, res)=>{})
+
+//post the order into the database
+app.post("/confirmation", (req, res)=>{})
+
+
 
 
 app.listen(()=>{
