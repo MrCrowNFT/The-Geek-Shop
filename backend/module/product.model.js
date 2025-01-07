44bd4de6903eb43fd911e62f8ea942e3a8ca3765
MrCrowNFT <mrcrownft@gmail.com>
2025-01-06 23:34:41 -0300

    Product schema

---
+++ backend/module/product.model.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+    {
+        name:{
+            type: String,
+            required: true,
+        },
+        //I want this to be calculated using the cost + shipping+ tax + profit - discount
+        priceTag:{
+            type:Number,
+            required: true,
+            min: 0,
+        },
+        cost:{
+            type:Number,
+            required: true,
+        },
+        shipping:{
+            type:Number,
+            required: true,
+        },
+        profit:{
+            type:Number,
+            required: true,
+        },
+        tax:{
+            type:Number,
+            required: true,
+        },
+        discount:{
+            amount: {type: Number, default:0},
+            status: {type: Boolean, default: false}
+        },
+        //sku for google analytics
+        sku:{
+            type: Number,
+            unique: true,
+        },
+        //this might change since it is for fetching the info of the products
+        url:[{
+            url: {type: String, required: true},
+            priority: {type: Number, required: true},
+        }],
+        images:[{
+            //have url of the image
+            type: String,
+            required:true,
+        }],
+        description:{
+            type: String,
+            required: true,
+            maxlength: 500,
+        },
+        //the validator still in progress
+        category:[{
+            type: String,
+            enum: ["Figure","Plushie"],
+            required: false,
+        }]
+    },
+    {
+        timestamps: true //createdAt, updatedAt
+    }
+)
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
