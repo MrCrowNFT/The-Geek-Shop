79a88b284aa44b139d8e8ee5492bcfeb6a9c5388
MrCrowNFT <mrcrownft@gmail.com>
2025-01-09 09:56:15 -0300

    user get product by id method

--- backend/main.js
+++ backend/main.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
   return res.status(200).send("Home page");
 });
 
-app.get("/products", async(req, res) => {
+app.get("/products", async (req, res) => {
     try{
         const products = await Product.find({});
         return res.status(200).json({success: true, data: products});
@@ -28,19 +28,32 @@ app.get("/products", async(req, res) => {
     }
 });
 
-app.get("/product/:id", (req, res) => {
-  const { id } = req.params;
-  //check if the product id is valid
-  //find the product
-  //return product
+app.get("/products/:id", async (req, res) => {
+    const {id} = req.params;
 
-  return res.status(200).send("Product");
+    //to catch 404 case 
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success: false, message: "Product not found"});
+    }
+
+    try{
+        //new: true so that it returns the updated product 
+        const product = await Product.findById(id);
+        return res.status(200).json({success: true, data: product});
+    }catch(error){
+        console.error(`Error updating product: ${error.message}`);
+        return res.status(500).json({success: false, message: "Server error"});
+    }
 });
+
+//*This methods need some work, need to get payment method thingy as well as  
+//*elavorate on the categories to make a search 
 
 //get search result page
 app.get("/products/search", (req, res) => {
   return res.status(200).send("Product");
 });
+
 //get checkout page
 app.get("/checkout", (req, res) => {});
 
