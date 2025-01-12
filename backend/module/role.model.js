0145f2388aa6558af1ba74f2089fd9cba7d35f4a
MrCrowNFT <mrcrownft@gmail.com>
2025-01-12 00:12:24 -0300

    Role model password hashing and comparing

--- backend/module/role.model.js
+++ backend/module/role.model.js
@@ -1,22 +1,46 @@
 import mongoose from "mongoose";
+import bycrypt from "bcrypt";
 
-const roleSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
+const roleSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    required: true,
-  },
-},
-{
-  timestamps: true,
+  {
+    timestamps: true,
+  }
+);
+//*can't use arrow functions cause i'm using 'this.password'
+
+//we will hash the password before storing it in the database
+//so we use the pre middleware hook that runs before saving
+roleSchema.pre("save", async function (next) {
+  try {
+    // checks whether the password field has been modified.
+    // and only hash the password if is new/modified
+    if (!this.isModified("password")) return next();
+    // salt 10 is provides good security while still being fast
+    this.password = await bycrypt.hash(this.password, 10);
+  } catch (error) {
+    next(error);
+  }
 });
+
+//compare entered password with the hashed password
+roleSchema.methods.comparePassword = async function (enteredPassword) {
+  return bycrypt.compare(enteredPassword, this.password);
+};
 
 const Role = mongoose.model("Role", roleSchema);
 export default Role;
