98537083408e3240f02c5c3e3c236a6cc85ef8a1
MrCrowNFT <mrcrownft@gmail.com>
2025-01-13 08:10:25 -0300

    category model

---
+++ backend/module/category.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      maxlength: 250,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Category = mongoose.model("Category", categorySchema);
+export default categorySchema;
