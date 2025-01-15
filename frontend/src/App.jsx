7fda402f776913204caeac6845d108c2497f14ea
MrCrowNFT <mrcrownft@gmail.com>
2025-01-15 09:41:53 -0300

    frontend directory layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
-
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Home.jsx";
 
 function App() {
   return (
     <>
-      
+      <Routes>
+				<Route path='/home' element={<HomePage></HomePage>} />
+				<Route path='/admin' element={} />
+			</Routes>
     </>
   )
 }
