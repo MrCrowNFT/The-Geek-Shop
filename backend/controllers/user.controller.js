4e13a7b169e3e28b555a4bba2dae6c6150a5660f
MrCrowNFT <mrcrownft@gmail.com>
2025-01-16 07:06:43 -0300

    user search method in controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -35,3 +35,40 @@ export const userGetProductById = async (req, res) => {
     return res.status(500).json({ success: false, message: "Server error" });
   }
 };
+
+//this method will be used for product search and advanced search, therfor
+//some params in the request query may be empty
+export const userSearch = async (req, res) => {
+  try {
+    const { categories, minPrice, maxPrice } = req.query;
+
+    //we'll use a dynamic query
+    const query = {};
+
+    if (categories) {
+      const categoriesIds = categories.split(","); //Parse coma separeted values
+      query.category = { $in: categoriesIds }; // filter
+    }
+
+    if (minPrice || maxPrice) {
+      query.priceTag = {};
+      if (minPrice) query.priceTag.$gte = Number(minPrice);
+      if (maxPrice) query.priceTag.$lte = Number(maxPrice);
+    }
+
+    //populate to get full category document from the Category collection. is it necesary though?
+    const products = await Product.find(query).populate("category");
+
+    if (!products.length) {
+      return res.status(404).json({
+        success: false,
+        message: "No products found matching the search criteria.",
+      });
+    }
+
+    res.status(200).json({ success: true, products });
+  } catch (error) {
+    console.error(`Error during product search: ${error.message}`);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
