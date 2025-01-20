c9453a62963d882cf84eed4a93e49ee3ec786c2a
MrCrowNFT <mrcrownft@gmail.com>
2025-01-20 07:09:16 -0300

    Header component jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,26 @@
-import React from "react";
 import { FaFacebook, FaTwitter, FaInstagram, FaEnvelope, FaUser, FaHeart, FaShoppingCart } from "react-icons/fa";
+import logo from "../assets/Logo(B).png"
 
 const Header = () => {
+
   return (
-    <Header>
-      
-    </Header>
+    <header>
+      <div className="social-media">
+        <FaFacebook></FaFacebook><FaTwitter></FaTwitter><FaInstagram></FaInstagram>
+        <p> | </p><FaEnvelope></FaEnvelope><a href="">marketplace@gmail.com</a>
+      </div>
+      <img src={logo} alt=""></img>
+      <div className="user-interactions">
+        <FaUser></FaUser>
+        <p>Login</p>
+        <p> | </p>
+        <p>+ Create</p>
+        <p> | </p>
+        <FaHeart></FaHeart>
+        <p> | </p>
+        <FaShoppingCart></FaShoppingCart>
+      </div>
+    </header>
   )}
     
 
