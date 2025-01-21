27967d8c49280319be5ed764633d14641035722b
MrCrowNFT <mrcrownft@gmail.com>
2025-01-21 09:17:27 -0300

    NavBar component

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,14 +1,60 @@
 import logo from "../assets/Logo(W).png"
+import search from "../assets/search.png"
+import cart from "../assets/shopping-cart.png"
 
 const Navbar = ()=>{
     return(
-        <>
-        <div className="nav">
+        <div className="navbar">
+            <div className="navbar-content">
+                <div className="navbar-content-left">
+                    <div className="logo-wrapper">
+                        <a href="/home">
+                            <img className="logo" loading="lazy" src={logo} alt="marketplace logo" />
+                        </a> 
+                    </div>
+                    <div className="nav-links">
+                        <nav>
+                            <ul>
+                                <li>
+                                    <a href="" className="nav-link">Figures</a>
+                                </li>
+                                <li>
+                                    <a href="" className="nav-link">Plushies</a>
+                                </li>
+                                <li>
+                                    <a href="" className="nav-link">Trading Cards</a>
+                                </li>
+                                <li>
+                                    <a href="" className="nav-link">Retro</a>
+                                </li>
+                                <li>
+                                    <a href="" className="nav-link">Goods</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div className="navbar-content-right">
+                    <div className="navbar-search-bar">
+                        <span className="search-icon-wrap">
+                            <img className="search-icon" src={search}></img>
+                        </span>
+                        <div className="search-bar">
+                            <form>
+                                <input className="search-query" type="text" placeholder="Search products" autoComplete="off" autoCapitalize="off"></input>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="cart">
+                        <button type="button" className="cart-button">
+                            <img className="cart-icon" src={cart}></img>
+                        </button>       
+                    </div>
+                </div>
+            </div>
 
-            <img id="logo" src={logo} alt="Marketplace Logo"></img>
-            <span className="navbar_separator"></span>
-            <a>Home</a>
-            </div></>
+        </div>
+        
     )
 }
 
