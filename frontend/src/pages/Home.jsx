65c28171a6558359fc913d14f4877d0662220540
MrCrowNFT <mrcrownft@gmail.com>
2025-01-23 06:52:03 -0300

    home page general layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,34 @@ const HomePage = () => {
     <>
       <Header />
       <Navbar></Navbar>
-      <div className="p-4">Welcome to the Marketplace!</div>
+      <div className="main">
+        <div className="main-content">
+          <div className="main-content-upper">
+            <a>
+              <img className="upper-banner" src="" alt="" />
+            </a>
+          </div>
+          <div className="main-content-lower">
+            <div className="main-content-lower-left">
+              <a>
+                <img className="left-upper-lower-banner" src="" alt="" />
+                <img className="left-lower-lower-banner" src="" alt="" />
+              </a>
+            </div>
+            <div className="main-content-lower-center">
+              <a>
+                <img className="middle-lower-banner" src="" alt="" />
+              </a>
+            </div>
+            <div className="main-content-lower-right">
+            <a>
+                <img className="right-upper-lower-banner" src="" alt="" />
+                <img className="right-lower-lower-banner" src="" alt="" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
