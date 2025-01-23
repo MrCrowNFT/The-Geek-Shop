71aedefa0372a676dbb01298843bd888c314dcef
MrCrowNFT <mrcrownft@gmail.com>
2025-01-23 12:19:32 -0300

    Banner Wrapper component for modulization

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import Header from "../components/Header/Header.jsx";
 import Navbar from "../components/NavBar/NavBar.jsx";
-import BannerA from "../assets/images/Testing-Banner(A)(2000x500).png";
-import BannerB from "../assets/images/Testing-Banner(B).png";
-import BannerC from "../assets/images/Testing-Banner(C).jpg";
-import BannerD from "../assets/images/Testing-Banner(D)(2000x1000).png";
+import BannerWrapper from "../components/BannerWrapper/BannerWrapper.jsx";
+
 import "./Home.css";
 
 const HomePage = () => {
@@ -11,68 +9,7 @@ const HomePage = () => {
     <>
       <Header />
       <Navbar></Navbar>
-      <div className="main">
-        <div className="main-content">
-          <div className="main-content-upper">
-            <a href="">
-              <img
-                className="upper-banner"
-                loading="lazy"
-                src={BannerA}
-                alt="Banner A"
-              />
-            </a>
-          </div>
-          <div className="main-content-lower">
-            <div className="main-content-lower-left">
-              <a href="">
-                <img
-                  className="left-upper-lower-banner"
-                  loading="lazy"
-                  src={BannerC}
-                  alt=""
-                />
-              </a>
-              <a href="">
-                <img
-                  className="left-lower-lower-banner"
-                  loading="lazy"
-                  src={BannerD}
-                  alt=""
-                />
-              </a>
-            </div>
-            <div className="main-content-lower-center">
-              <a href="">
-                <img
-                  className="middle-lower-banner"
-                  loading="lazy"
-                  src={BannerB}
-                  alt=""
-                />
-              </a>
-            </div>
-            <div className="main-content-lower-right">
-              <a href="">
-                <img
-                  className="right-upper-lower-banner"
-                  loading="lazy"
-                  src={BannerD}
-                  alt=""
-                />
-              </a>
-              <a href="">
-                <img
-                  className="right-lower-lower-banner"
-                  loading="lazy"
-                  src={BannerC}
-                  alt=""
-                />
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <BannerWrapper></BannerWrapper>
     </>
   );
 };
