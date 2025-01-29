0be14cebe2903d239f810ebcc91c7ad9322f381d
MrCrowNFT <mrcrownft@gmail.com>
2025-01-29 09:34:01 -0300

    pagination added to search controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -17,13 +17,15 @@ export const userGetProducts = async (req, res) => {
 };
 
 export const userGetProductById = async (req, res) => {
-  const { id } = req.params;  
+  const { id } = req.params;
 
   try {
     //new: true so that it returns the updated product
     const product = await Product.findById(id);
     if (!product) {
-      return res.status(404).json({ success: false, message: "Product not found" });
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
     }
     return res.status(200).json({ success: true, data: product });
   } catch (error) {
@@ -32,11 +34,18 @@ export const userGetProductById = async (req, res) => {
   }
 };
 
-//this method will be used for product search and advanced search, therfor
+//this method will be used for product search therefor
 //some params in the request query may be empty
 export const userSearch = async (req, res) => {
   try {
-    const { categories, minPrice, maxPrice } = req.query;
+    const {
+      categories,
+      minPrice,
+      maxPrice,
+      searchTerm,
+      page = 1,
+      limit = 20, //20 products per page
+    } = req.query;
 
     //we'll use a dynamic query
     const query = {};
@@ -52,8 +61,24 @@ export const userSearch = async (req, res) => {
       if (maxPrice) query.priceTag.$lte = Number(maxPrice);
     }
 
+    // Text search (if searchTerm is provided)
+    if (searchTerm) {
+      query.$text = { $search: searchTerm }; // MongoDB text search
+    }
+
+    // Convert page and limit to numbers, base 10
+    const pageNumber = parseInt(page, 10);
+    const limitNumber = parseInt(limit, 10);
+    const skip = (pageNumber - 1) * limitNumber; // Calculate the number of documents to skip
+
     //populate to get the full product data
-    const products = await Product.find(query).populate("category");
+    const products = await Product.find(query)
+      .populate("category")
+      .skip(skip)
+      .limit(limitNumber);
+
+    //get total num of products returns for pagination
+    const totalProducts = Product.countDocuments(query);
 
     if (!products.length) {
       return res.status(404).json({
@@ -62,7 +87,16 @@ export const userSearch = async (req, res) => {
       });
     }
 
-    res.status(200).json({ success: true, data: products });
+    res.status(200).json({
+      success: true,
+      data: products,
+      pagination: {
+        totalProducts, //total amount of products
+        totalPages: Math.ceil(totalProducts / limitNumber), //total amount of pages
+        currentPage: pageNumber, // current page
+        productsPerPage: limitNumber, // number of products per page
+      },
+    });
   } catch (error) {
     console.error(`Error during product search: ${error.message}`);
     res.status(500).json({ success: false, message: "Server error" });
--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,16 +1,21 @@
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import Navbar from "../components/NavBar/NavBar";
+import ProductCard from "../components/ProductCard/ProductCard";
 
 //this will recieve the products requested from the query
-const SearchPage = () => {
+const SearchPage = (products) => {
   return (
     <>
         <Header/>
         <Navbar/>
         <div className="content">
-            <div></div>  
-            <div></div>  
+            <div className="search-options"></div>  
+            <div className="result-products">
+                {products.slice(0, 20).map((product) => (
+                          <ProductCard key={product.id} product={product} />
+                        ))}
+                </div>  
         </div>
         <Footer/>
     </>
