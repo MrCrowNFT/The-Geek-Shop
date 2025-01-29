1e8cdc5e09d3c35fce4e6949396c3d8e8763a87b
MrCrowNFT <mrcrownft@gmail.com>
2025-01-29 10:07:18 -0300

    Search path on app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home.jsx";
 import ProductPage from "./pages/ProductPage.jsx";
+import SearchPage from "./pages/SearchPage.jsx";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/home" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
         <Route path="/product" element={<ProductPage/>}/>
       </Routes>
     </>
