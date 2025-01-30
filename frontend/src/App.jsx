f8277e20aa9d1941d604bfe22cb844b19b79ef61
MrCrowNFT <mrcrownft@gmail.com>
2025-01-30 08:20:23 -0300

    material ui added for admin dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home.jsx";
 import ProductPage from "./pages/ProductPage.jsx";
 import SearchPage from "./pages/SearchPage.jsx";
+import AdminDashboard from "./pages/AdminDashboard.jsx";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Routes>
         <Route path="/home" element={<HomePage />} />
         <Route path="/search" element={<SearchPage />} />
-        <Route path="/product" element={<ProductPage/>}/>
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </>
   );
