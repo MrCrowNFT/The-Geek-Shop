d2a8b7bd044c9d3ef32e5a7e4d10bb3256153993
MrCrowNFT <mrcrownft@gmail.com>
2025-02-03 07:32:13 -0300

    Product List Item component

--- frontend/src/components/ProductListItem/ProductListItem.jsx
+++ frontend/src/components/ProductListItem/ProductListItem.jsx
@@ -1,7 +1,28 @@
-import "./ProducListItem.css"
+import "./ProducListItem.css";
+import PropTypes from "prop-types";
 
-const ProducListItem = ()=>{
-    return (<></>)
-}
+const ProducListItem = (product) => {
+  return (
+    <div className="product">
+      <a href="">
+        <div className="product-name">
+          <img src={product.images[0]} alt={product.name} />
+          <p>{product.name}</p>
+          <p>${product.price}</p>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+ProducListItem.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }).isRequired,
+};
 
 export default ProducListItem;
