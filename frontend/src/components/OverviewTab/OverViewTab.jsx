b19b6fe8c00f99b4c35f66969755982b4892d099
MrCrowNFT <mrcrownft@gmail.com>
2025-02-06 06:42:29 -0300

    Overview Tab init

---
+++ frontend/src/components/OverviewTab/OverViewTab.jsx
@@ -0,0 +1,12 @@
+import "./OverViewTab.css";
+import Indicator from "../Indicator/Indicator";
+
+const OverViewTab = () => {
+  return (
+  <div>
+    <Indicator/>
+  </div>
+  );
+};
+
+export default OverViewTab;
