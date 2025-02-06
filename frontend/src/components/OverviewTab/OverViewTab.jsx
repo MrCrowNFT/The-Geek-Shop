ac74a54d05c88860cd369d5cb97a3ee07fcedec5
MrCrowNFT <mrcrownft@gmail.com>
2025-02-06 08:25:44 -0300

    recent orders overview tab

--- frontend/src/components/OverviewTab/OverViewTab.jsx
+++ frontend/src/components/OverviewTab/OverViewTab.jsx
@@ -1,6 +1,8 @@
 import "./OverViewTab.css";
 import Indicator from "../Indicator/Indicator.jsx";
 import BarGraph from "../BarGraph/BarGraph.jsx";
+import { useEffect, useState } from "react";
+import OrderListItem from "../OrderListItem/OrderListItem.jsx";
 
 const mockAmount = [
   {
@@ -17,9 +19,35 @@ const mockAmount = [
     index: "Yearly earnings",
     num: 299.99,
     trend: "",
-  }
+  },
 ];
 const OverViewTab = () => {
+  const [orders, setOrders] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  //need to adjust this for only the most recent ones
+  const fetchOrders = async (page = 1, limit = 20) => {
+    setLoading(true);
+    try {
+      //need to check the route!!
+      //Need to check how this works with the JWT
+      const response = await fetch(
+        `/admin/orders/search?page=${page}&limit=${limit}`
+      );
+      const data = await response.json();
+      if (data.success) {
+        setOrders(data.data);
+      }
+    } catch (error) {
+      console.error("Error fetching orders:", error);
+    } finally {
+      //setting load off
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchOrders();
+  });
   return (
     <div className="overview-tab">
       <div className="indicators">
@@ -27,11 +55,19 @@ const OverViewTab = () => {
         <Indicator amount={mockAmount[1]} />
         <Indicator amount={mockAmount[2]} />
       </div>
-      <br/>
+      <br />
       <div className="lower-info">
-        <div className="performance-graph"></div>
-        <div>
+        <div className="performance-graph">
           <BarGraph />
+        </div>
+        <div className="recent-orders">
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            orders.map((product) => (
+              <OrderListItem key={product._id} product={product} />
+            ))
+          )}
         </div>
       </div>
     </div>
