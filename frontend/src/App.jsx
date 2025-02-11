ba99c8dd067adc2c33cceca4858b0c324aa980d2
MrCrowNFT <mrcrownft@gmail.com>
2025-02-11 06:43:43 -0300

    Query client added to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,21 @@ import HomePage from "./pages/Home.jsx";
 import ProductPage from "./pages/ProductPage.jsx";
 import SearchPage from "./pages/SearchPage.jsx";
 import AdminDashboard from "./pages/AdminDashboard.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/product" element={<ProductPage />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/product" element={<ProductPage />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+        </Routes>
+      </QueryClientProvider>
     </>
   );
 }
