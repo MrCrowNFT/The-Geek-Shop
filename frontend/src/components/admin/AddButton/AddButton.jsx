65c6f2b2329738f8a3d31f688e3e64d9ce91d92e
MrCrowNFT <mrcrownft@gmail.com>
2025-02-12 09:29:47 -0300

    Add New Product Button

--- frontend/src/components/admin/AddButton/AddButton.jsx
+++ frontend/src/components/admin/AddButton/AddButton.jsx
@@ -1,7 +1,30 @@
 import "./AddButton.css";
+import NewProduct from "../NewProduct/NewProduct.jsx";
+import { useState } from "react";
 
 const AddButtom = () => {
-  return <button></button>;
+  const [showNewProduct, setShowNewProduct] = useState(false);
+
+  const toggleNewProduct = () => setShowNewProduct((prev) => !prev);
+
+  return (
+    <div>
+      <button className="open-modal-button" onClick={toggleNewProduct}>
+        Add New Product
+      </button>
+
+      {showNewProduct && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <button className="close-modal-button" onClick={toggleNewProduct}>
+              Close
+            </button>
+            <NewProduct />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default AddButtom;
