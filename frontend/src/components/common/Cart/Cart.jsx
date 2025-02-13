8c01205aab81ff4172df788ec5d486783321a103
MrCrowNFT <mrcrownft@gmail.com>
2025-02-13 09:40:40 -0300

    cart fix

--- frontend/src/components/common/Cart/Cart.jsx
+++ frontend/src/components/common/Cart/Cart.jsx
@@ -1,32 +1,29 @@
-import "./Cart.css"
-import {useCart} from "../../../hooks/useCart.js"
+import "./Cart.css";
+import { useCart } from "../../../hooks/useCart.js";
 
-const Cart = ()=>{
+const Cart = () => {
+  //get the methods from the useCart Hook
+  //the addToCart will be on the product button
+  const { cartItems, removeFromCart, clearCart } = useCart;
 
-    //get the methods from the useCart Hook
-    //the addToCart will be on the product button
-    const { cartItems, removeFromCart, clearCart} = useCart;
-    
-    return (
-        <div className="cart-container">
-          <h2>Your Cart</h2>
-          {cartItems.length === 0 ? (
-            <p>Your cart is empty</p>
-          ) : (
-            <ul>
-              {cartItems.map((item) => (
-                <li key={item.id}>
-                  {item.name} - ${item.price}
-                  <button onClick={() => removeFromCart(item.id)}>Remove</button>
-                </li>
-              ))}
-            </ul>
-          )}
-          <button onClick={clearCart}>Clear Cart</button>
-        </div>
-      );
-    };
-
-}
+  return (
+    <div className="cart-container">
+      <h2>Your Cart</h2>
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        <ul>
+          {cartItems.map((item) => (
+            <li key={item.id}>
+              {item.name} - ${item.price}
+              <button onClick={() => removeFromCart(item.id)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      )}
+      <button onClick={clearCart}>Clear Cart</button>
+    </div>
+  );
+};
 
 export default Cart;
