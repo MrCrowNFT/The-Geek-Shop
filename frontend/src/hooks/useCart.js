6c1ce00fc3d3e4e2fd7d01aa36c436f7c79ae759
MrCrowNFT <mrcrownft@gmail.com>
2025-02-13 09:10:55 -0300

    useCart Hook

---
+++ frontend/src/hooks/useCart.js
@@ -0,0 +1,34 @@
+import { createContext, useContext, useState } from "react";
+import PropType from "prop-types";
+import PropTypes from "prop-types";
+
+const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product) => {
+    setCartItems((prevItems) => [...prevItems, product]);
+  };
+
+  //removing items by id
+  const removeFromCart = (productId) => {
+    setCartItems((prevItems) =>
+      prevItems.filter((item) => item.id !== productId)
+    );
+  };
+
+  const clearCart = () => setCartItems([]);
+
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart, clearCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+CartProvider.propType ={
+    //this means children must be anything renderable in React 
+    children: PropTypes.node.isRequired,
+}
+
+export const useCart = () => useContext(CartContext);
