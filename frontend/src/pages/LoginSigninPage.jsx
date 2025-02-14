550387c96055c4b5cae8166c19b06e8b5356bab0
MrCrowNFT <mrcrownft@gmail.com>
2025-02-14 16:55:07 -0300

    Login Signup page

---
+++ frontend/src/pages/LoginSigninPage.jsx
@@ -0,0 +1,18 @@
+import "./LoginSigninPage.css";
+import LoginSigninTabs from "../components/common/LoginSigninTabs.jsx";
+import Header from "../components/common/Header/Header.jsx";
+import Navbar from "../components/common/NavBar/NavBar.jsx";
+import Footer from "../components/common/Footer/Footer.jsx";
+
+const LoginSigninPage = () => {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <LoginSigninTabs />
+      <Footer />
+    </>
+  );
+};
+
+export default LoginSigninPage;
