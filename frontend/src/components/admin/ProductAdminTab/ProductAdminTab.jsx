da8280fcd1de0e7c39b244c03f7c0865ab46b986
MrCrowNFT <mrcrownft@gmail.com>
2025-02-24 16:13:05 -0300

    Product tab refactor

--- frontend/src/components/admin/ProductAdminTab/ProductAdminTab.jsx
+++ frontend/src/components/admin/ProductAdminTab/ProductAdminTab.jsx
@@ -1,61 +1,29 @@
-import { useState, useEffect } from "react";
+import { useProducts } from "../../../hooks/useProducts.js";
 import ProducListItem from "../ProductListItem/ProductListItem.jsx";
-import "./ProductAdminTab.css";
 import AddButtom from "../AddButton/AddButton.jsx";
+import "./ProductAdminTab.css";
 
 const ProductAdminTab = () => {
-  const [products, setProducts] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-  const [loading, setLoading] = useState(false); //to display while loading
-
-  //fetch products
-  const fetchProducts = async (page = 1, limit = 20) => {
-    setLoading(true);
-    try {
-      //need to check the route!!
-      const response = await fetch(`/home/search?page=${page}&limit=${limit}`);
-      const data = await response.json();
-      if (data.success) {
-        setProducts(data.data);
-        setTotalPages(data.pagination.totalPages);
-      }
-    } catch (error) {
-      console.error("Error fetching products:", error);
-    } finally {
-      //setting load off
-      setLoading(false);
-    }
-  };
-  // fetch products when the component mounts or the page changes
-  useEffect(() => {
-    fetchProducts(currentPage);
-  }, [currentPage]);
-
-  // page nac handlers
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const handlePreviousPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
+  const {
+    products,
+    loading,
+    currentPage,
+    totalPages,
+    handleNextPage,
+    handlePreviousPage,
+  } = useProducts();
 
   return (
     <div className="product-tab">
-      <AddButtom/>
+      <AddButtom />
       <div className="product-list">
         {loading ? (
-                    <p>Loading...</p>
-                  ) : (
-                    products.map((product) => (
-                      <ProducListItem key={product._id} product={product} />
-                    ))
-                  )}
+          <p>Loading...</p>
+        ) : (
+          products.map((product) => (
+            <ProducListItem key={product._id} product={product} />
+          ))
+        )}
       </div>
       <div className="pagination">
         <button onClick={handlePreviousPage} disabled={currentPage === 1}>
@@ -71,4 +39,5 @@ const ProductAdminTab = () => {
     </div>
   );
 };
+
 export default ProductAdminTab;
