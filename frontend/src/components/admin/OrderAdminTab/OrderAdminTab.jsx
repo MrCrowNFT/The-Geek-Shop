011d7b5e2d38b334425187429a966f6f0317f440
MrCrowNFT <mrcrownft@gmail.com>
2025-02-24 16:28:16 -0300

    Order tab refactor

--- frontend/src/components/admin/OrderAdminTab/OrderAdminTab.jsx
+++ frontend/src/components/admin/OrderAdminTab/OrderAdminTab.jsx
@@ -1,59 +1,24 @@
-import { useState, useEffect } from "react";
+import { useOrders } from "../../../hooks/useOrders";
 import OrderListItem from "../OrderListItem/OrderListItem.jsx";
 import "./OrderAdminTab.css";
 
 const OrderAdminTab = () => {
-  const [orders, setOrders] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-  const [loading, setLoading] = useState(false);
+  const {
+    orders,
+    loading,
+    currentPage,
+    totalPages,
+    handleNextPage,
+    handlePreviousPage,
+  } = useOrders();
 
-  const fetchOrders = async (page = 1, limit = 20) => {
-    setLoading(true);
-    try {
-      //need to check the route!!
-      //Need to check how this works with the JWT
-      const response = await fetch(
-        `/admin/orders/search?page=${page}&limit=${limit}`
-      );
-      const data = await response.json();
-      if (data.success) {
-        setOrders(data.data);
-        setTotalPages(data.pagination.totalPages);
-      }
-    } catch (error) {
-      console.error("Error fetching orders:", error);
-    } finally {
-      //setting load off
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchOrders(currentPage);
-  }, [currentPage]);
-
-  // page nac handlers
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const handlePreviousPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
   return (
     <div className="orders-tab">
       <div className="orders-list">
         {loading ? (
           <p>Loading...</p>
         ) : (
-          orders.map((product) => (
-            <OrderListItem key={product._id} product={product} />
-          ))
+          orders.map((order) => <OrderListItem key={order._id} order={order} />)
         )}
       </div>
       <div className="pagination">
