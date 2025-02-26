2099120a16439d7ff4201685e0dbf16013c495c0
MrCrowNFT <mrcrownft@gmail.com>
2025-02-26 17:17:36 -0300

    stripe backend integration

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,31 @@ import adminRouter from "./routes/admin.routes.js";
 import userRouter from "./routes/user.routes.js";
 import categoryRouter from "./routes/category.routes.js";
 import cors from "cors";
+import Stripe from "stripe";
 
 const app = express();
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 app.use(express.json()); //accept JSON files
 app.use(cors()); // Allow all origins: probably should change it just specific one
-
 
 app.use("/home", userRouter);
 app.use("/admin", adminRouter);
 app.use("/categories", categoryRouter);
 
-export default app;
+app.post("/create-payment-intent", async (req, res) => {
+  try {
+    const { amount, currency } = req.body;
 
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency,
+    });
+
+    res.json({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+export default app;
